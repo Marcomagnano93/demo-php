f6e8a02cbd3d83552332ca9d4692bae8da8c0950
Marcomagnano93 <marcog.magnano@gmail.com>
2024-07-30 17:53:42 +0200

    ADD: arrays & associative arrays

--- index.php
+++ index.php
@@ -8,13 +8,32 @@
 <body>
     <!-- PHP CONDITIONS -->
     <?php 
+    // array
     $names = [
         "Luca",
         "Paolo",
         "Marco"
     ];
+    // associative array
+    $people = [
+            [
+                'name' => 'Gianni',
+                'age' => 22,
+                'adress' => "Milano"
+            ],
+            [
+                'name' => 'Mario',
+                'age' => 56,
+                'adress' => "Torino"
+            ],
+            [
+                'name' => 'Alessia',
+                'age' => 21,
+                'adress' => "Palermo"
+            ]
+        ];
 
-
+    // if / else
     $condition = true;
 
 
@@ -23,18 +42,31 @@
     } else {
         $message = "No!";
     };
+    
     ?>
 
+    <!-- PHP Dynamic message -->
     <h1>
         <?= $message ?>
     </h1>
 
     <!-- PHP Array & foreach -->
+     <h2>Array</h2>
      <ol>
         <?php foreach ($names as $name) : ?>
         <li><?= $name ?></li>
         <?php endforeach ?>
      </ol>
+    
+     <h2>Array associativo</h2>
+     <h5>Persone:</h5>
+     <ul>
+        <?php foreach ($people as $person) : ?>
+            <li>Nome: <?= $person['name']; ?></li>
+            <li>Età: <?= $person['age']; ?></li>
+            <li>Indirizzo: <?= $person['adress']; ?></li> <hr>
+        <?php endforeach ?>
 
+     </ul>
 </body>
 </html>
