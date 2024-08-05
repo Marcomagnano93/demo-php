2ca9cadf90c16ef826e0c8f2cb9a047522dd4992
Marcomagnano93 <marcog.magnano@gmail.com>
2024-08-05 18:13:20 +0200

    ADD: links dynamic style

--- index.php
+++ index.php
@@ -2,4 +2,15 @@
 
 $heading = 'home';
 
+function dd ($value) {
+    echo '<pre>';
+
+    var_dump($value);
+
+    echo '</pre>';
+
+    die();
+}
+
+// dd($_SERVER);
 require "index.view.php";
--- partials/nav.php
+++ partials/nav.php
@@ -1,6 +1,6 @@
 <div class="navcontainer">
             <ul class="navbar">
-                <li><a href="/index.php">Home</a></li>
-                <li><a href="/exercise.php">Esercizi</a></li>
+                <li><a href="/index.php" class="link <?php if ($_SERVER['REQUEST_URI'] === '/index.php') { echo 'currentlink';} ?>">Home</a></li>
+                <li><a href="/exercise.php" class="link <?php if ($_SERVER['REQUEST_URI'] === '/exercise.php') { echo 'currentlink';} ?>">Esercizi</a></li>
             </ul>
 </div>
