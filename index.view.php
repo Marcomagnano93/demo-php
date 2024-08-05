40c9a2b3c2e4d8895791ecc6cf1b1908c7d183b4
Marcomagnano93 <marcog.magnano@gmail.com>
2024-08-05 17:43:56 +0200

    ADD: partials head, navbar

--- index.view.php
+++ index.view.php
@@ -1,22 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo</title>
-    <link rel="stylesheet" href="/css/style.css">
-</head>
+<?php require 'partials/head.php' ?>
 <body>
     <header>
         <div>
             <h1 class="logo">Sito</h1>
         </div>
-        <div class="navcontainer">
-            <ul class="navbar">
-                <li><a href="/index.php">Home</a></li>
-                <li><a href="/exercise.php">Esercizi</a></li>
-            </ul>
-        </div>
+<?php require 'partials/nav.php' ?>
     </header>
     <main>
 
---
+++ partials/nav.php
@@ -0,0 +1,6 @@
+<div class="navcontainer">
+            <ul class="navbar">
+                <li><a href="/index.php">Home</a></li>
+                <li><a href="/exercise.php">Esercizi</a></li>
+            </ul>
+</div>
