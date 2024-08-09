1f081d6579cc99090ad9b703064eea66abd147c9
Marcomagnano93 <marcog.magnano@gmail.com>
2024-08-09 10:38:20 +0200

    REFACT router, ADD page 404

--- index.php
+++ index.php
@@ -3,11 +3,30 @@ require 'functions.php';
 
 $uri = $_SERVER['REQUEST_URI'];
 
-if ($uri === '/'){
-    require 'controllers/index.php';
+
+
+// if ($uri === '/'){
+//     require 'controllers/index.php';
+// }
+// elseif ($uri === '/exercise') {
+//     require 'controllers/exercise.php';
+// }
+
+//router
+$routes = [
+    '/' => 'controllers/index.php',
+    '/exercise' => 'controllers/exercise.php'
+];
+
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+} else {
+    http_response_code(404);
+
+    require 'view/404.php';
+
+    die();
 }
-elseif ($uri === '/exercise') {
-    require 'controllers/exercise.php';
-}
+
 
 // dd($_SERVER);
